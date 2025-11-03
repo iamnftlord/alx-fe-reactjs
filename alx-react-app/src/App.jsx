e21aa55f8856e0d47292b75456885be99c67b568
iamnftlord <afolabigbenga1995@gmail.com>
2025-11-03 09:24:30 +0100

    update

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -12,10 +12,10 @@ function App() {
 
   return (
     <>
-    <WelcomeMessage/>
-    <Header/>
-    <MainContent/>
-    <Footer/>
+    <WelcomeMessage />
+    <Header />
+    <MainContent />
+    <Footer />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
