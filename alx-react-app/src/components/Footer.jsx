ddb5ec61f41c5d77b83583ec55a91d86373f215a
iamnftlord <afolabigbenga1995@gmail.com>
2025-11-09 06:23:03 +0100

    styling

--- alx-react-app/src/components/Footer.jsx
+++ alx-react-app/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer(){
     return(
-        <footer>
+        <footer style={{color: 'navy', textAlign: 'center'}}>
             <p>© 2023 City Lovers</p>
         </footer>
     );
--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -1,6 +1,6 @@
 function MainContent() {
     return (
-        <main>
+        <main style={{color: 'purple', textWeight: 'bold', fontSize: '30px'}}>
             <p>I love to visit New York, Paris, and Tokyo.</p>
         </main>
     );
