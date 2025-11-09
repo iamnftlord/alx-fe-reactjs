e82611775acaa58e9853a9adad7b234ec35b28d9
iamnftlord <afolabigbenga1995@gmail.com>
2025-11-09 18:09:36 +0100

    updated

--- alx-react-app-new/src/components/counter.jsx
+++ alx-react-app-new/src/components/counter.jsx
@@ -5,7 +5,7 @@ function Counter(){
 
     return(
         <div>
-            <p>You clicked {count} times</p>
+            <p>Current Count: {count}</p>
             <button onClick={()=> setCount(count+1)}>Increment</button>
             <button onClick={()=> setCount(count-1)}>Decrement</button>
             <button onClick={()=> setCount(0)}>Reset</button>
