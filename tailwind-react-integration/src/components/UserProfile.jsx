34da6ab056fadd08dba88a62017c34b8523a6d1b
iamnftlord <afolabigbenga1995@gmail.com>
2025-12-01 05:19:23 +0100

    latest

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,17 +1,17 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-sm mx-auto my-10 rounded-lg shadow-lg">
+    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs  sm:max-w-sm mx-auto my-10 rounded-lg shadow-lg">
       <img
-        className="rounded-full w-24 h-24 sm:w-28 sm:h-28 md:w-36 md:h-36 mx-auto object-cover"
+        className="rounded-full w-24 h-24 md:max-w-sm sm:w-28 sm:h-28 md:w-36 md:h-36 mx-auto object-cover"
         src="https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg"
         alt="User"
       />
 
-      <h1 className="text-lg sm:text-xl md:text-2xl text-blue-800 my-4 text-center">
+      <h1 className="text-lg sm:text-xl md:max-w-sm md:text-2xl text-blue-800 my-4 text-center">
         John Doe
       </h1>
 
-      <p className="text-gray-600 text-sm sm:text-base md:text-lg text-center">
+      <p className="text-gray-600 text-sm sm:text-base md:max-w-sm md:text-lg text-center">
         Developer at Example Co. Loves to write code and explore new technologies.
       </p>
     </div>
