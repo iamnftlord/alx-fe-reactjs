0f27e42520c5cd93e1a9839ed0cc0ce0f66afd4d
iamnftlord <afolabigbenga1995@gmail.com>
2025-12-01 05:06:59 +0100

    Responsive Design

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,9 +1,9 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
-      <img className="rounded-full w-36 h-36 mx-auto" src="https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg" alt="User" />
-      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-      <p className="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
+    <div className="bg-gray-100 p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-8 md:my-20 rounded-lg shadow-lg">
+      <img className="rounded-full w-24 h-24 md:h-36 mx-auto object-cover" src="https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg" alt="User" />
+      <h1 className="text-lg md:text-xl text-blue-800 my-4 text-center">John Doe</h1>
+      <p className="text-gray-600 text-sm md:text-base text-cente">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
 }
