50ef1b82b8aa98b2c17a22662d4b8f444f8b0048
iamnftlord <afolabigbenga1995@gmail.com>
2025-12-16 05:02:03 +0100

    update

--- react-todo/src/TodoList.test.js
+++ react-todo/src/TodoList.test.js
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent } from "@testing-library/react";
-import TodoList from "../components/TodoList";
+import TodoList from "./components/TodoList";
 
 test("renders initial todos", () => {
   render(<TodoList />);
